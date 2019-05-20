fdd5bf56eb944fc3a3158a0eff3fdc8f0d4fc21c
Abraham Matus <aimatusco@gmail.com>
2019-05-20 16:45:27 -0500

    Adds default 404 page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
 import Header from './components/ui/header/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import PageNotFound from './components/page-not-found/PageNotFound';
 
 function App() {
   return (
     <div className="container-fluid">
       <Header />
-      <Route exact path="/" component={HomePage} />
-      <Route path="/about" component={AboutPage} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route component={PageNotFound} />
+      </Switch>
     </div>
   );
 }
