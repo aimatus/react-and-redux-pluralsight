dee984cf30c9f97e80acc8ed2b4bfb1e7120d9ae
Abraham Matus <aimatusco@gmail.com>
2019-05-20 16:19:05 -0500

    Adds a header to the app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
+import Header from './components/ui/header/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="container-fluid">
+      <Header />
       <Route exact path="/" component={HomePage} />
       <Route path="/about" component={AboutPage} />
     </div>
