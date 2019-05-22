4d14397a458a2a0311eb6a2c6f2c3bdc41b1a9f2
Abraham Matus <aimatusco@gmail.com>
2019-05-22 17:06:14 -0500

    Adds an add course form

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,8 +1,30 @@
 import React from 'react';
 
 class CoursesPage extends React.Component {
+  state = {
+    course: {
+      title: ''
+    }
+  };
+
+  handleChange = event => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course }); // Object shorthand syntax instead of { course: course }
+  };
+
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <form>
+        <h2>Courses</h2>
+        <h3>Add Course</h3>
+        <input
+          type=""
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="Save" />
+      </form>
+    );
   }
 }
 
