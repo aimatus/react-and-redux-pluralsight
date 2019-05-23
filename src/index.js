b1d9664f596169643bfb736d836b448b6afaa635
Abraham Matus <aimatusco@gmail.com>
2019-05-23 11:32:03 -0500

    Adds missing Redux store to index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider as ReduxProvider } from 'react-redux';
+import configureStore from './redux/configureStore';
+
+const store = configureStore();
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>,
   document.getElementById('root')
 );
 
