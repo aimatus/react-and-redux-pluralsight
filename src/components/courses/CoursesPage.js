e0e0334fceb9488cd5510429a0b9ecb90034d92e
Abraham Matus <aimatusco@gmail.com>
2019-05-23 11:19:51 -0500

    Connects CoursesPage to Redux

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import * as courseActions from '../../redux/actions/courseActions';
+import PropTypes from 'prop-types';
 
 class CoursesPage extends React.Component {
   state = {
@@ -14,7 +17,7 @@ class CoursesPage extends React.Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    alert(this.state.course.title);
+    this.props.dispatch(courseActions.createCourse(this.state.course));
   };
 
   render() {
@@ -33,4 +36,14 @@ class CoursesPage extends React.Component {
   }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+  dispatch: PropTypes.func.isRequired
+};
+
+function mapStateToPros(state) {
+  return {
+    courses: state.courses
+  };
+}
+
+export default connect(mapStateToPros)(CoursesPage);
