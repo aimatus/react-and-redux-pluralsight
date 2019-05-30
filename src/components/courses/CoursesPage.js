235f5bbd6a746a5f728c278d42eebec034bb5351
Abraham Matus <aimatusco@gmail.com>
2019-05-30 17:44:18 -0500

    Cleans Courses page to be just a container

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -5,37 +5,14 @@ import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 
 class CoursesPage extends React.Component {
-  state = {
-    course: {
-      title: ''
-    }
-  };
-
-  handleChange = event => {
-    const course = { ...this.state.course, title: event.target.value };
-    this.setState({ course }); // Object shorthand syntax instead of { course: course }
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    this.props.actions.createCourse(this.state.course);
-  };
-
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <>
         <h2>Courses</h2>
-        <h3>Add Course</h3>
-        <input
-          type=""
-          onChange={this.handleChange}
-          value={this.state.course.title}
-        />
-        <input type="submit" value="Save" />
         {this.props.courses.map(course => (
           <div key={course.title}>{course.title}</div>
         ))}
-      </form>
+      </>
     );
   }
 }
