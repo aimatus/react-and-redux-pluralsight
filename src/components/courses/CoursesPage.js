68269f90de6c104d8bc9424326f3e376d2291e10
Abraham Matus <aimatusco@gmail.com>
2019-06-06 08:15:52 -0500

    Creates courses list page

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import * as courseActions from '../../redux/actions/courseActions';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
+import CourseList from './CoursesList';
 
 class CoursesPage extends React.Component {
   componentDidMount() {
@@ -15,9 +16,7 @@ class CoursesPage extends React.Component {
     return (
       <>
         <h2>Courses</h2>
-        {this.props.courses.map(course => (
-          <div key={course.title}>{course.title}</div>
-        ))}
+        <CourseList courses={this.props.courses} />
       </>
     );
   }
